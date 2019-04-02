7bc9c9be1d3d2277d54e48381628c76cedd89531
Filip Jerga <jerga99@gmail.com>
2019-04-03 00:10:19 +0100

    13 Moves App to Src

--- App.vue
+++ App.vue
@@ -1,43 +1,12 @@
 <template>
-  <view class="container">
-    <AppNavigation />
-  </view>
+  <App />
 </template>
 
 <script>
-  import HomeScreen from '@/screens/HomeScreen'
-  import Screen1 from '@/screens/Screen1'
-  import Screen2 from '@/screens/Screen2'
-
-  import { createStackNavigator, createAppContainer } from 'react-navigation'
-
-  const HomeStack = createStackNavigator(
-    {
-      Home: HomeScreen,
-      ScreenOne: Screen1,
-      ScreenTwo: Screen2
-    },
-    {
-      initialRouteName: 'Home'
-    }
-  )
-
-  const AppNavigation = createAppContainer(HomeStack)
-
+  import App from './src'
   export default {
     components: {
-      HomeScreen, AppNavigation
+      App
     }
   }
 </script>
-
-<style>
-  .container {
-    background-color: white;
-    flex: 1;
-    padding: 40px;
-  }
-  .text-color-primary {
-    color: blue;
-  }
-</style>
