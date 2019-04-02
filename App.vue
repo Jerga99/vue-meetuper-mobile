892d71a37fde0846efb7f5ffc3beee3e4a1df401
Filip Jerga <jerga99@gmail.com>
2019-04-02 23:49:15 +0100

    11 React Navigation Stack Implementation

--- App.vue
+++ App.vue
@@ -1,14 +1,32 @@
 <template>
   <view class="container">
-    <HomeScreen />
+    <AppNavigation />
   </view>
 </template>
 
 <script>
   import HomeScreen from '@/screens/HomeScreen'
+  import Screen1 from '@/screens/Screen1'
+  import Screen2 from '@/screens/Screen2'
+
+  import { createStackNavigator, createAppContainer } from 'react-navigation'
+
+  const HomeStack = createStackNavigator(
+    {
+      Home: HomeScreen,
+      Screen1: Screen1,
+      Screen2: Screen2
+    },
+    {
+      initialRouteName: 'Home'
+    }
+  )
+
+  const AppNavigation = createAppContainer(HomeStack)
+
   export default {
     components: {
-      HomeScreen
+      HomeScreen, AppNavigation
     }
   }
 </script>
@@ -16,9 +34,8 @@
 <style>
   .container {
     background-color: white;
-    align-items: center;
-    justify-content: center;
     flex: 1;
+    padding: 40px;
   }
   .text-color-primary {
     color: blue;
---
+++ src/screens/Screen2.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>
+    <text class="text-color-primary">{{title}}</text>
+  </view>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: 'I AM SCREEN 2'
+      }
+    }
+  }
+</script>
