0fbf3ff229640fc16457938a6a087dc4255b461a
Filip Jerga <jerga99@gmail.com>
2019-04-04 22:52:09 +0100

    26 Meetup Card Separate Component

---
+++ src/components/MeetupCard.vue
@@ -0,0 +1,65 @@
+
+
+
+<template>
+  <nb-card>
+    <nb-card-item bordered>
+      <nb-left>
+        <nb-thumbnail :source="{uri: meetup.meetupCreator.avatar}"></nb-thumbnail>
+        <nb-body>
+          <nb-text>{{meetup.title}}</nb-text>
+          <nb-text note>{{meetup.startDate}}</nb-text>
+        </nb-body>
+      </nb-left>
+    </nb-card-item>
+    <nb-card-item>
+      <nb-body>
+        <image :source="{uri: meetup.image}"
+               :style="stylesObj.cardItemImage"
+               class="card-item-image"/>
+        <nb-text>{{meetup.description}}</nb-text>
+      </nb-body>
+    </nb-card-item>
+    <nb-card-item :style="{ paddingVertical: 0 }">
+      <nb-left>
+        <nb-button transparent>
+          <nb-icon name="person"></nb-icon>
+          <nb-text>{{meetup.joinedPeopleCount}} people are comming</nb-text>
+        </nb-button>
+      </nb-left>
+    </nb-card-item>
+  </nb-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      meetup: {
+        type: Object,
+        required: true
+      }
+    },
+    data () {
+      return {
+        stylesObj: {
+          cardItemImage: {
+            resizeMode: "cover",
+            width: '100%',
+            marginBottom: 10
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .card-item-image {
+    flex: 1;
+    height: 200;
+  }
+</style>
+
+
+
+
--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,39 +1,20 @@
 <template>
   <scroll-view>
     <nb-text class="header-1">Featured Meetups</nb-text>
-    <nb-card v-for="meetup in meetups">
-      <nb-card-item bordered>
-        <nb-left>
-          <nb-thumbnail :source="{uri: meetup.meetupCreator.avatar}"></nb-thumbnail>
-          <nb-body>
-            <nb-text>{{meetup.title}}</nb-text>
-            <nb-text note>{{meetup.startDate}}</nb-text>
-          </nb-body>
-        </nb-left>
-      </nb-card-item>
-      <nb-card-item>
-        <nb-body>
-          <image :source="{uri: meetup.image}"
-                 :style="stylesObj.cardItemImage"
-                 class="card-item-image"/>
-          <nb-text>{{meetup.description}}</nb-text>
-        </nb-body>
-      </nb-card-item>
-      <nb-card-item :style="{ paddingVertical: 0 }">
-        <nb-left>
-          <nb-button transparent>
-            <nb-icon name="person"></nb-icon>
-            <nb-text>{{meetup.joinedPeopleCount}} people are comming</nb-text>
-          </nb-button>
-        </nb-left>
-      </nb-card-item>
-    </nb-card>
+    <!-- Separate This -->
+    <MeetupCard v-for="meetup in meetups"
+                :meetup="meetup"
+                :key="meetup._id" />
+    <!-- Separate This -->
   </scroll-view>
 </template>
 
 <script>
-  import axios from 'axios'
+  import MeetupCard from '@/components/MeetupCard'
   export default {
+    components: {
+      MeetupCard
+    },
     props: {
       navigation: {
         type: Object
@@ -41,14 +22,7 @@
     },
     data () {
       return {
-        title: 'Home Screen!',
-        stylesObj: {
-          cardItemImage: {
-            resizeMode: "cover",
-            width: '100%',
-            marginBottom: 10
-          }
-        }
+        title: 'Home Screen!'
       }
     },
     computed: {
@@ -73,11 +47,6 @@
 </script>
 
 <style>
-  .card-item-image {
-    flex: 1;
-    height: 200;
-  }
-
   .header-1 {
     font-size: 23px;
     padding: 20px;
