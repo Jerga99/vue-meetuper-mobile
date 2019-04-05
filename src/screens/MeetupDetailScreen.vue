612257576f40a80018bb8a179e1c8a87d332bbe1
Filip Jerga <jerga99@gmail.com>
2019-04-05 22:42:21 +0100

    32 Meetup Detail Info Component

--- src/screens/MeetupDetailScreen.vue
+++ src/screens/MeetupDetailScreen.vue
@@ -18,65 +18,19 @@
         <nb-card-item header bordered>
           <nb-text>Details</nb-text>
         </nb-card-item>
-        <nb-card-item bordered>
-          <nb-body>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              Date
-            </nb-text>
-            <nb-text>
-              {{meetup.startDate}}
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              From
-            </nb-text>
-            <nb-text>
-              {{meetup.timeFrom}}
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              To
-            </nb-text>
-            <nb-text>
-              {{meetup.timeTo}}
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              Category
-            </nb-text>
-            <nb-text>
-              {{meetup.category.name}}
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              Info
-            </nb-text>
-            <!-- short info -->
-            <nb-text>
-              {{meetup.shortInfo}}
-            </nb-text>
-          </nb-body>
-        </nb-card-item>
-        <nb-card-item header bordered>
-          <nb-text>Description</nb-text>
-        </nb-card-item>
-        <nb-card-item bordered>
-          <nb-body>
-            <!-- description -->
-            <nb-text>
-              {{meetup.description}}
-            </nb-text>
-          </nb-body>
-        </nb-card-item>
+        <MeetupDetailInfo :meetup="meetup"/>
       </nb-card>
     </nb-content>
   </nb-container>
 </template>
 
 <script>
+  import MeetupDetailInfo from '@/components/MeetupDetailInfo'
   import styles from '@/styles'
   export default {
+    components: {
+      MeetupDetailInfo
+    },
     props: {
       navigation: {
         type: Object
