b53472fc14028814e4bffa62fe60e46bddc4d00b
Filip Jerga <jerga99@gmail.com>
2019-04-07 00:28:28 +0100

    40 Login, Register + Navigation

--- src/navigation.vue
+++ src/navigation.vue
@@ -7,6 +7,9 @@
 <script>
   import HomeScreen from '@/screens/HomeScreen'
   import MeetupDetailScreen from '@/screens/MeetupDetailScreen'
+  import LoginScreen from '@/screens/LoginScreen'
+  import RegisterScreen from '@/screens/RegisterScreen'
+
   import Screen1 from '@/screens/Screen1'
   import Screen2 from '@/screens/Screen2'
   import Screen3 from '@/screens/Screen3'
@@ -15,6 +18,7 @@
   import { createStackNavigator,
            createBottomTabNavigator,
            createDrawerNavigator,
+           createSwitchNavigator,
            createAppContainer } from 'react-navigation'
 
   const HomeStack = createStackNavigator(
@@ -27,6 +31,11 @@
       initialRouteName: 'Home'
     }
   )
+
+  const AuthStack = createStackNavigator({
+    Login: LoginScreen,
+    Register: RegisterScreen
+  })
 
   const SomeOtherStack = createStackNavigator({
     Screen2
@@ -56,7 +65,10 @@
     Other: DrawerNavigation
   })
 
-  const AppNavigation = createAppContainer(TabNavigation)
+  const AppNavigation = createAppContainer(createSwitchNavigator({
+    auth: AuthStack,
+    tabs: TabNavigation
+  }))
 
   export default {
     components: {
