8fdd72308a4bdb47fd89dfeee91000b5ed9f127e
Filip Jerga <jerga99@gmail.com>
2019-04-07 21:09:56 +0100

    41 Login Screen Bind Data

--- src/screens/LoginScreen.vue
+++ src/screens/LoginScreen.vue
@@ -10,24 +10,52 @@
     <nb-content padder>
       <nb-form>
         <nb-item>
-          <nb-input placeholder="Email" auto-capitalize="none"/>
+          <nb-input v-model="form.email"
+                    placeholder="Email"
+                    auto-capitalize="none"/>
         </nb-item>
         <nb-item last>
-          <nb-input placeholder="Password" auto-capitalize="none" secure-text-entry />
+          <nb-input v-model="form.password"
+                    placeholder="Password"
+                    auto-capitalize="none"
+                    secure-text-entry />
         </nb-item>
       </nb-form>
       <view :style="{marginTop:10}">
-        <nb-button block>
-          <nb-text>Login </nb-text>
+        <nb-button :on-press="login" block>
+          <nb-text>Login</nb-text>
         </nb-button>
       </view>
+      <nb-button transparent :on-press="goToRegister">
+        <nb-text>Not Registered? Register Here</nb-text>
+      </nb-button>
     </nb-content>
   </nb-container>
 </template>
 
 <script>
   export default {
-
+    props: {
+      navigation: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        form: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      login () {
+        alert(`${JSON.stringify(this.form)}`)
+      },
+      goToRegister () {
+        this.navigation.navigate('Register')
+      }
+    }
   }
 </script>
 
