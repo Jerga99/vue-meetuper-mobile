9880d4915d4ab5ba086b53e11459d05c6b8235ff
Filip Jerga <jerga99@gmail.com>
2019-04-07 21:41:32 +0100

    42 Register Data Bining

--- src/screens/RegisterScreen.vue
+++ src/screens/RegisterScreen.vue
@@ -10,35 +10,78 @@
     <nb-content padder>
       <nb-form>
         <nb-item>
-          <nb-input placeholder="Username" auto-capitalize="none" />
+          <nb-input v-model="form.username"
+                    placeholder="Username"
+                    auto-capitalize="none" />
         </nb-item>
         <nb-item>
-          <nb-input placeholder="Full Name" auto-capitalize="none" />
+          <nb-input v-model="form.name"
+                    placeholder="Full Name"
+                    auto-capitalize="none" />
         </nb-item>
         <nb-item>
-          <nb-input placeholder="Email" auto-capitalize="none" />
+          <nb-input v-model="form.email"
+                    placeholder="Email"
+                    auto-capitalize="none" />
         </nb-item>
         <nb-item>
-          <nb-input placeholder="Avatar Url" auto-capitalize="none" />
+          <nb-input v-model="form.avatar"
+                    placeholder="Avatar Url"
+                    auto-capitalize="none" />
         </nb-item>
         <nb-item>
-          <nb-input placeholder="Password" auto-capitalize="none" secure-text-entry />
+          <nb-input v-model="form.password"
+                    placeholder="Password"
+                    auto-capitalize="none"
+                    secure-text-entry />
         </nb-item>
         <nb-item>
-          <nb-input last placeholder="Password Confirmation" auto-capitalize="none" />
+          <nb-input v-model="form.passwordConfirmation"
+                    last
+                    placeholder="Password Confirmation"
+                    auto-capitalize="none" />
         </nb-item>
       </nb-form>
       <view :style="{marginTop:10}">
-        <nb-button block>
+        <nb-button :on-press="register" block>
           <nb-text>Register</nb-text>
         </nb-button>
       </view>
+      <nb-button transparent :on-press="goToLogin">
+        <nb-text>Already Registered? You can login here.</nb-text>
+      </nb-button>
     </nb-content>
   </nb-container>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: {
+      navigation: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        form: {
+          username: '',
+          name: '',
+          avatar: '',
+          email: '',
+          password: '',
+          passwordConfirmation: ''
+        }
+      }
+    },
+    methods: {
+      register () {
+        alert(JSON.stringify(this.form))
+      },
+      goToLogin () {
+        this.navigation.navigate('Login')
+      }
+    }
+  }
 </script>
 
 
