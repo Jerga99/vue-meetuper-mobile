af85ebaef23a202c0636976e98b3488f648bb380
Filip Jerga <jerga99@gmail.com>
2019-04-11 21:58:14 +0100

    66 Bind Data to Meetup Create form

--- src/screens/MeetupCreateScreen.vue
+++ src/screens/MeetupCreateScreen.vue
@@ -16,41 +16,41 @@
         <nb-form>
           <nb-item stackedLabel>
             <nb-label>Location</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.location"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Title</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.title"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Start Date</nb-label>
-            <nb-input />
+            <nb-input v-model="form.startDate"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Time From</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.timeFrom"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Time To</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.timeTo"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Category</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.category"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Image</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.image"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Description</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.description"/>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Additional Info</nb-label>
-            <nb-input/>
+            <nb-input v-model="form.shortInfo"/>
           </nb-item>
-          <nb-button block>
+          <nb-button :on-press="createMeetup" block>
             <nb-text>Create Meetup</nb-text>
           </nb-button>
         </nb-form>
@@ -73,8 +73,30 @@
     },
     data () {
       return {
-        styles
+        styles,
+        form: {
+          location: null,
+          title: null,
+          startDate: null,
+          category: null,
+          image: null,
+          shortInfo: null,
+          description: null,
+          timeTo: null,
+          timeFrom: null
+        }
+      }
+    },
+    methods: {
+      createMeetup () {
+        alert(JSON.stringify(this.form))
       }
     }
   }
 </script>
+
+
+
+
+
+
