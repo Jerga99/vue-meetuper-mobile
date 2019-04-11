f1aa9285780e1e8e8844f9f86b12cd6b9eae3cd8
Filip Jerga <jerga99@gmail.com>
2019-04-11 22:28:38 +0100

    67 Category Picker

--- src/screens/MeetupCreateScreen.vue
+++ src/screens/MeetupCreateScreen.vue
@@ -36,7 +36,25 @@
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Category</nb-label>
-            <nb-input v-model="form.category"/>
+            <view :style="{flex: 1,
+                           alignSelf: 'stretch',
+                           paddingLeft: null,
+                           marginLeft: null,
+                           heigth: 50}">
+              <nb-picker
+                mode="dropdown"
+                placeholder="Select Category"
+                placeholderStyle="{ color: '#bfc6ea' }"
+                :selectedValue="form.category"
+                :onValueChange="onCategoryChange"
+              >
+                <nb-item label="Wallet" value="key0" />
+                <nb-item label="ATM Card" value="key1" />
+                <nb-item label="Debit Card" value="key2" />
+                <nb-item label="Credit Card" value="key3" />
+                <nb-item label="Net Banking" value="key4" />
+              </nb-picker>
+            </view>
           </nb-item>
           <nb-item stackedLabel>
             <nb-label>Image</nb-label>
@@ -90,6 +108,9 @@
     methods: {
       createMeetup () {
         alert(JSON.stringify(this.form))
+      },
+      onCategoryChange (category) {
+        this.form.category = category
       }
     }
   }
