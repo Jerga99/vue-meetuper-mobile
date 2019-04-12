4322da33e77383418812b533981c8613cceecb42
Filip Jerga <jerga99@gmail.com>
2019-04-12 23:21:18 +0100

    77 Prod Base URL

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,8 +2,20 @@ import axios from 'axios'
 import { AsyncStorage } from 'react-native'
 import { Platform } from 'react-native'
 
-const BASE_URL = Platform.OS === 'ios' ? 'http://localhost:3001/api/v1'
-                                       : 'http://10.0.2.2:3001/api/v1'
+const BASE_URL = __DEV__ ? (Platform.OS === 'ios' ? 'http://localhost:3001/api/v1'
+                                                  : 'http://10.0.2.2:3001/api/v1')
+                         : 'https://vue-meetuper-server.herokuapp.com/api/v1'
+
+// let BASE_URL
+
+// if (__DEV__) {
+//   BASE_URL = Platform.OS === 'ios' ? 'http://localhost:3001/api/v1'
+//                                    : 'http://10.0.2.2:3001/api/v1'
+// } else {
+//   BASE_URL = 'https://vue-meetuper-server.herokuapp.com/api/v1'
+// }
+
+
 const axiosInstance = axios.create({
   timeout: 3000,
   baseURL: BASE_URL
